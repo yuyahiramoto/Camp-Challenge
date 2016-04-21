25caa51554246af1f4fa8db24caf71a7301ac5ba
yuyahiramoto <yuyahi0212@gmail.com>
2016-04-21 13:19:07 +0900

    PHPプログラミング基礎学習1  課題：基礎編 1-3まで

--- challenge/challenge_1/1_1challenge.php
+++ challenge/challenge_1/1_1challenge.php
@@ -1,3 +1,10 @@
 <?php
-	echo "Hello world!!";
+
+
+	//課題:基礎編 １．「Hello world.」を表示してください。
+	
+	//Hello world.を表示
+	echo "Hello world.";
+	
+	
 ?>
---
+++ challenge/challenge_1/1_3challenge.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>自己紹介</title>
+
+
+</head>
+<body>
+
+<!--課題:基礎編 ３．自分の自己紹介を作成してみてください。-->
+
+<p>自己紹介をします。</p>
+
+	<p>初めまして、私の名前は
+
+	<?php
+	  echo "平本裕也";
+	?>
+
+	です。
+	</p>
+
+
+
+</body>
+</html>
+	
+
