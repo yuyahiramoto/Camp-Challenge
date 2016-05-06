ee9e1a19bd0764aa94618860e237a88868432739
yuyahiramoto <yuyahi0212@gmail.com>
2016-05-06 13:37:54 +0900

    PHPプログラミング基礎学習2 課題：基礎編8 追加修正

--- challenge/challenge_2/2_8challenge.php
+++ challenge/challenge_2/2_8challenge.php
@@ -9,10 +9,17 @@ $arr = array();
     // array(0,1,2,3.....)
 $arr = array(10,100,'soeda','hayashi',-20,118,'END');
 
+//配列を表示する。
+for ($i =0; $i < count($arr); $i++){
+  echo $arr[$i] . "<br>";
+}
+
 //配列の 'soeda'を 33 に上書きする。
 $arr[2] = 33;
 
 //上書きされたことを確認する。
-echo $arr[2];
+for ($i =0; $i < count($arr); $i++){
+  echo $arr[$i] . "<br>";
+}
 
 ?>
